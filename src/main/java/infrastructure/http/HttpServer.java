511debcf156d616a59611ea3621b4f616649c40c
DongKey777 <higgs95@naver.com>
2025-11-13 16:46:19 +0900

    feat(http): 요청 파싱 기능 분리
    - 헤더 파싱 기능 추가
    - handleClient 메서드 분리

--- src/main/java/infrastructure/http/HttpServer.java
+++ src/main/java/infrastructure/http/HttpServer.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HttpServer {
 
@@ -14,18 +16,45 @@ public class HttpServer {
 
         while (true) {
             Socket client = server.accept();
-            System.out.println("클라이언트 연결됨");
 
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(client.getInputStream())
-            );
-
-            String requestLine = in.readLine();
-            System.out.println("요청: " + requestLine);
-
-            HttpRequest request = HttpRequest.from(requestLine);
-
-            client.close();
+            try {
+                handleClient(client);
+            } catch (IOException e) {
+                System.out.println("오류" + e.getMessage());
+            } finally {
+                client.close();
+            }
         }
+    }
+
+    private void handleClient(Socket client) throws IOException {
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(client.getInputStream())
+        );
+
+        String requestLine = in.readLine();
+
+        HttpRequest httpRequest = HttpRequest.from(requestLine);
+        Map<String, String> headers = parseHeaders(in);
+    }
+
+    private Map<String, String> parseHeaders(BufferedReader in) throws IOException {
+        Map<String, String> headers = new HashMap<>();
+
+        while (true) {
+            String line = in.readLine();
+
+            if (line == null || line.isEmpty()) {
+                break;
+            }
+
+            int delimiter = line.indexOf(':');
+            if (delimiter > 0) {
+                String key = line.substring(0, delimiter).trim();
+                String value = line.substring(delimiter + 1).trim();
+                headers.put(key, value);
+            }
+        }
+        return headers;
     }
 }
