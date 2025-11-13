8bc0024c1d6e9ec9fcf355977fa78e094b074f63
DongKey777 <higgs95@naver.com>
2025-11-13 15:06:43 +0900

    feat(http): http 상태 객체 분리

---
+++ src/main/java/infrastructure/http/HttpStatus.java
@@ -0,0 +1,19 @@
+package infrastructure.http;
+
+public enum HttpStatus {
+    OK(200, "OK"),
+    NOT_FOUND(404, "Not Found"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error");
+
+    private final int code;
+    private final String message;
+
+    HttpStatus(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public String toStatusLine() {
+        return "HTTP/1.1 " + code + " " + message;
+    }
+}
